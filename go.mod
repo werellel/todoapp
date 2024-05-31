76849310ef5151dd29c2730e3d6bb215e0b911bb
werellel <werellel@gmail.com>
2024-05-31 17:07:51 +0900

    feat: add NotifyContext

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/todoapp
 
 go 1.22.1
 
-require github.com/caarlos0/env/v6 v6.10.1 // indirect
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	golang.org/x/sync v0.7.0
+)
--- main.go
+++ main.go
@@ -4,7 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/todoapp/config"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -13,13 +21,39 @@ func main() {
 	}
 }
 
-func run(_ context.Context) error {
+func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
-	s.ListenAndServe()
-	return nil
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.Serve(l); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	return eg.Wait()
 }
