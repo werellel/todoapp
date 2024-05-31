b7eba66742fa61ce67be7764dd852a898a4bc93a
werellel <werellel@gmail.com>
2024-05-31 14:53:31 +0900

    feat: add etc

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	if err := run(context.Background()); err != nil {
+		log.Printf("fail to terminate server: %v", err)
+	}
+}
+
+func run(_ context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Printf("fail to terminate server: %v", err)
-		os.Exit(1)
 	}
+	s.ListenAndServe()
+	return nil
 }
